22824efee49d8516d6ca8bb6ff44725967871432
michwiesner <michywiesner@gmail.com>
2023-11-20 13:01:44 -0300

    changes package

--- app.ts
+++ app.ts
@@ -7,11 +7,10 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 3001;
 
 app.get("/health", (_: Request, res: Response) => res.status(200).json("OK"));
-app.get("/list-accounts", list);
+app.get("/", list);
 
-app.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}`)
+app.listen(process.env.PORT || 3001, () =>
+  console.log(`Server running at http://localhost:${process.env.PORT || 3001}`)
 );

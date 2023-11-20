c7347aa710e7497a86e35a29bfa31f61488cae74
michwiesner <michywiesner@gmail.com>
2023-11-20 12:37:28 -0300

    changes package

--- app.ts
+++ app.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 app.get("/health", (_: Request, res: Response) => res.status(200).json("OK"));
 app.get("/list-accounts", list);
